e0f643af4d82247fcd7f6b179f35eeadd4495c0d
mrana565 <rana565m@gmail.com>
2023-03-04 20:27:43 +0600

    show all data in card

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ const fetchAllData = (dataLength) => {
     .then((data) => allData(data.data.tools, dataLength));
 };
 fetchAllData(6);
+
+const allData = (datas, dataLength) => {
+  const allDataSection = document.getElementById("all-data");
+  allDataSection.innerHTML = "";
+
+ 
+
+  datas.forEach((data) => {
+    const { image, features, name, published_in, id } = data;
+
+    allDataSection.innerHTML += `
+        <div class="col-sm-6 col-lg-4">
+            <div class="card bg-body-secondary">
+                <img style="height:200px;" src="${image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="card-title">Features</h2>
+                    <p class="card-text">1. ${features[0]}</p>
+                    <p class="card-text">2. ${features[1]}</p>
+                    <p class="card-text">3. ${features[2]}</p>
+                </div>
+                <hr>
+                <div class="d-flex justify-content-between align-items-center p-3">
+                    <div>
+                        <h4>${name}</h4>
+                        <p>${published_in}</p>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary" onclick="fetchMoreDetail('${id}')" data-bs-toggle="modal" data-bs-target="#more-data">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+  });
+  spinner(false);
+};
